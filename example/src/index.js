59851226231e430ac32f34970bc5963ef104852b
evont <evontgoh@foxmail.com>
2021-09-06 23:47:47 +0800

    feat: use squoosh lib to convert image to webp

--- example/src/index.js
+++ example/src/index.js
@@ -1,1 +1,18 @@
 import "./assets/style/index.css";
+
+function testWebp(callback) {
+  const image = new Image();
+  image.onerror = function () {
+    callback(false);
+  };
+  image.onload = function () {
+    callback(image.width == 1);
+  };
+  image.src =
+    "data:image/webp;base64,UklGRiwAAABXRUJQVlA4ICAAAAAUAgCdASoBAAEAL/3+/3+CAB/AAAFzrNsAAP5QAAAAAA==";
+}
+
+testWebp((flag) => {
+    document.body.classList.add(flag ? 'webp' : 'nowebp')
+  
+})
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,7 +1,10 @@
 import { Declaration, Node, PluginCreator, Rule } from "postcss";
 import valueParser, { FunctionNode } from "postcss-value-parser";
-import { transformAlias, startsWith} from "./util";
-
+import { transformAlias, startsWith } from "./util";
+import * as path from "path";
+import * as fs from "fs-extra";
+import { ImagePool } from "@squoosh/lib";
+import { url } from "inspector";
 export default ({ loaderContext, options }) => {
   let { outputPath, className } = options;
 
@@ -26,10 +29,25 @@ export default ({ loaderContext, options }) => {
       }
     }
   }
-  const result: Record<string, {
-    url: string[],
-    decl: Declaration
-  }> = {};
+  const result: Record<
+    string,
+    {
+      url: string[];
+      decl: Declaration;
+      webpDecl: Declaration;
+    }
+  > = {};
+  const targets = {
+    ".png": "oxipng",
+    ".jpg": "mozjpeg",
+    ".jpeg": "mozjpeg",
+    ".jxl": "jxl",
+    ".webp": "webp",
+    ".avif": "avif",
+    // ...minifyOptions.targets,
+  };
+  console.log(outputPath, realOutput);
+  fs.ensureDirSync(realOutput);
   const PostcssPlugin: PluginCreator<{}> = function () {
     return {
       postcssPlugin: "webp-connvert-parser",
@@ -38,35 +56,86 @@ export default ({ loaderContext, options }) => {
           const { selector } = decl.parent as Rule;
           result[selector] = Object.assign(result[selector] || {}, {
             url: result[selector]?.url || [],
-            decl
-          })
+            decl,
+            webpDecl: undefined,
+          });
           const { nodes } = valueParser(decl.value);
-          nodes.forEach((ele) => {
-            if (ele.value === 'url') {
-             const [ urlNode ] = (ele as FunctionNode).nodes;
-             const url = urlNode.value;
-             result[selector].url.push(url)
+          for (const node of nodes) {
+            if (node.value === "url") {
+              const [urlNode] = (node as FunctionNode).nodes;
+              const url = urlNode.value;
+              result[selector].url.push(url);
+              try {
+                const imagePath = await new Promise<string>((resolve, reject) =>
+                  loaderContext.resolve(
+                    loaderContext.context,
+                    url,
+                    (err, result) => (err ? reject(err) : resolve(result))
+                  )
+                );
+                const ext = path.extname(imagePath).toLowerCase();
+                const targetCodec = targets[ext];
+                if (!targetCodec) {
+                  throw new Error(
+                    `The "${imagePath}" was not minified, ${ext} extension is not supported".`
+                  );
+                }
+                const encodeOptions = {
+                  [targetCodec]: {},
+                  webp: {},
+                  // ...minifyOptions.encodeOptions,
+                };
+                const imagePool = new ImagePool();
+                const image = imagePool.ingestImage(imagePath);
+                await image.decoded;
+                await image.preprocess({
+                  quant: {
+                    numColors: 256,
+                    dither: 0.5,
+                  },
+                });
+                await image.encode(encodeOptions);
+                //TODO: compare image size
+                const rawImage = (await image.encodedWith[targetCodec]).binary;
+                const rawImageInWebp = (await image.encodedWith.webp).binary;
+                fs.outputFileSync(imagePath, rawImage);
+                fs.outputFileSync(imagePath + ".webp", rawImageInWebp);
+                await imagePool.close();
+              } catch (err) {
+                console.error(err);
+                console.error(`${url} is not found`);
+              }
             }
-          })
+          }
         }
       },
       OnceExit(root, { Rule }) {
         //console.log(root)
         const nowebps = [];
-        const webps = []
+        const webps = [];
         for (const item in result) {
-          const norule = new Rule({ selector: `.${className.nowebp} ${item}`,  })
+          const norule = new Rule({ selector: `.${className.nowebp} ${item}` });
           norule.append(result[item].decl);
           nowebps.push(norule);
 
-          const rule = new Rule({ selector: `.${className.webp} ${item}`,  })
-          rule.append(result[item].decl);
-          nowebps.push(rule);
+          let finalValue = result[item].decl.value;
+          result[item].url.forEach((url) => {
+            finalValue = finalValue.replace(url, url + ".webp");
+          });
+          result[item].webpDecl = result[item].decl.clone({
+            value: finalValue,
+          });
+
+          console.log(result[item].webpDecl);
+
+          const rule = new Rule({ selector: `.${className.webp} ${item}` });
+          rule.append(result[item].webpDecl);
+          webps.push(rule);
         }
-        root.append([...nowebps, ...webps])
-      }
+        root.append([...nowebps, ...webps]);
+      },
     };
-  }
+  };
   PostcssPlugin.postcss = true;
   return {
     PostcssPlugin,
